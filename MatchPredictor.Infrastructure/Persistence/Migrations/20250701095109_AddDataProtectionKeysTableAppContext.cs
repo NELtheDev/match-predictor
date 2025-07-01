94fe516ce6b212062effe96ce91e8e697c703b3a
Nnamdi <nnamdi.okafor@stanbicibtc.com>
2025-07-01 11:16:37 +0100

    Update migration

--- MatchPredictor.Infrastructure/Persistence/Migrations/20250701095109_AddDataProtectionKeysTableAppContext.cs
+++ MatchPredictor.Infrastructure/Persistence/Migrations/20250701095109_AddDataProtectionKeysTableAppContext.cs
@@ -10,13 +10,19 @@ namespace MatchPredictor.Infrastructure.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"
+                CREATE TABLE IF NOT EXISTS dataprotectionkeys (
+                    id SERIAL PRIMARY KEY,
+                    friendlyname TEXT NOT NULL,
+                    xml TEXT NOT NULL
+                );
+            ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DROP TABLE IF EXISTS dataprotectionkeys;");
         }
     }
 }
